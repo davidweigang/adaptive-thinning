ec08479ee504dc78451ce598ceaf805ee446b9b6
Nako11 <dawei2@mailbox.org>
2020-07-12 18:58:22 +0200

    pkg

---
+++ pkg/pkg/wasm_game_of_life_bg.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_wasmimage_free(a: number): void;
+export function wasmimage_new(a: number, b: number, c: number, d: number, e: number, f: number): number;
+export function wasmimage_width(a: number): number;
+export function wasmimage_height(a: number): number;
+export function wasmimage_psnr(a: number): number;
+export function wasmimage_n_remaining(a: number): number;
+export function wasmimage_image_pixels(a: number): number;
+export function wasmimage_encoded_bytes(a: number): number;
+export function wasmimage_point_image_pixels(a: number): number;
+export function wasmimage_triangulation_image_pixels(a: number): number;
+export function wasmimage_thinning(a: number): void;
+export function __wbindgen_malloc(a: number): number;
