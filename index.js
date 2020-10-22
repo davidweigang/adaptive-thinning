9058e5f2fd1aca275829c38b083c3a32d9d9c3ec
Nako11 <dawei2@mailbox.org>
2020-10-22 21:36:59 +0200

    bla

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ ctx.drawImage(img, 0, 0);
 let img0_url = c.toDataURL("image/png");
 let imgData = ctx.getImageData(0,0,c.width, c.height);
 
-let at_image = AtEncoder.new(0, c.width, c.height, 32, imgData.data);
+let at_image = AtEncoder.new(0, c.width, c.height, 33, imgData.data);
 //let at_image = AtEncoder.new(74000, c.width, c.height, 33, imgData.data);
 console.log(at_image.width());
 console.log(at_image.height());
